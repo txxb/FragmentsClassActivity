d0797437f786efdc4a58cf8894d2c06916bccf85
Maab Taha <tmaab13@gmail.com>
2025-03-13 11:32:12 -0400

    changing color through spinner

--- app/src/main/java/edu/temple/fragments/ColorFragment.kt
+++ app/src/main/java/edu/temple/fragments/ColorFragment.kt
@@ -1,10 +1,13 @@
 package edu.temple.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 
@@ -15,12 +18,36 @@ class ColorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_color, container, false).apply {
+        return inflater.inflate(R.layout.fragment_color, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.apply {
             findViewById<Spinner>(R.id.spinner).apply {
                 adapter = ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item,
                     arrayOf("Blue", "Red", "White", "Black", "Cyan"))
+
+                onItemSelectedListener = object: OnItemSelectedListener{
+                    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                        p0?.run{
+                            view.setBackgroundColor(Color.parseColor(getItemAtPosition(p2).toString()))
+                        }
+                    }
+
+                    override fun onNothingSelected(p0: AdapterView<*>?) {
+                        TODO("Not yet implemented")
+                    }
+                }
+
             }
         }
+
+
     }
 
+
 }
+
